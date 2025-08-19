5b85c9a8ecdc04dc80452cb789fa49aa1935a515
EdiGal91 <47693118+EdiGal91@users.noreply.github.com>
2025-08-19 21:31:47 +0400

    remove unused variable

--- frontend/src/components/AdminPanel.tsx
+++ frontend/src/components/AdminPanel.tsx
@@ -28,7 +28,6 @@ export const AdminPanel: React.FC<AdminPanelProps> = ({
     }
 
     const isFullWithdrawal = !amount;
-    const amountToWithdraw = amount || currentBalance;
 
     if (
       !isFullWithdrawal &&
